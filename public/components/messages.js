b1fcb5daaf9f0d05a79c31672eef30061a64595a
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-11-22 13:21:01 +0100

    Bring back animation while waiting

--- public/components/messages.js
+++ public/components/messages.js
@@ -4,5 +4,21 @@ import Message from "./message.js";
 export default function Messages({ msgs }) {
   const content = msgs.map((msg) => React.createElement(Message, { msg }));
 
+  // Check if the last message is from the user
+  const lastMessage = msgs[msgs.length - 1];
+  const isWaitingForResponse = lastMessage && lastMessage.role === "user";
+
+  // If waiting, add a loading indicator
+  if (isWaitingForResponse) {
+    const waitingMessage = React.createElement(
+      "div",
+      { className: "message assistant waiting" },
+      React.createElement("span", null, "●"),
+      React.createElement("span", null, "●"),
+      React.createElement("span", null, "●"),
+    );
+    content.push(waitingMessage);
+  }
+
   return React.createElement(React.Fragment, null, content);
 }
